9f3a987bcc5177054d48673048f1c58b77b9badd
Nicholas Schiraldi <nschiraldi@albany.edu>
2018-08-28 10:23:36 -0400

    First attempt to refresh the nodes

--- barclay-simple.js
+++ barclay-simple.js
@@ -1,8 +1,4 @@
 /* ######################## Specify default stuff here ###############*/
-// plot width/height
-var w = 0;
-h = 0;
-
 // colorbar palletes
 var defaultPalette = ["rgb(234,95,95)", "rgb(58,32,154)", "rgb(255,177,78)", "rgb(255,242,0)", "rgb(117,250,122)", "rgb(0,155,123)", "rgb(140,62,174)", "rgb(154,154,154)", "rgb(0,0,0)"]
 var CBPalette = ["#c57b5e", "#15387f", "#f6b752", "#ffed86", "#cce59d", "#649084", "#595aa9", "#a69a9e", "#000000"]
@@ -36,15 +32,12 @@ var networkChart = {
 
 /* ###################### Data Loading Block ###################### */
 // load the force data
-var forceData = $.ajax({
-  url: "force-revised.json",
-  dataType: "json",
-  success: "data loaded",
-  error: function(xhr) {
-    alert(xhr.statusText)
-  },
-  cache: false
-});
+var forceData = $.getJSON('force-revised.json',
+  function(d) {
+    buildNetwork(d);
+    return d.responseJSON
+  }
+);
 /* ####################################################################*/
 
 // filter function, for year selection
@@ -65,8 +58,8 @@ function filterDataByYear(data, year) {
   sources = output['nodes'].map(x => x['name'])
 
   // take only the links to a given source
-  for (var i=0; i < data['links'].length;i++){
-    if ((sources.includes(data['links'][i]['source'])) && (sources.includes(data['links'][i]['target']))){
+  for (var i = 0; i < data['links'].length; i++) {
+    if ((sources.includes(data['links'][i]['source'])) && (sources.includes(data['links'][i]['target']))) {
       output['links'].push(data['links'][i])
     }
   }
@@ -83,61 +76,99 @@ function buildNetwork(data) {
 
 };
 
-function drawNetwork() {
+// $.when(forceData).done(
+//   function(x){
+//     buildNetwork(x)
+//     // mouse double click highlight is taken from this example:
+//     // http://www.coppelia.io/2014/06/finding-neighbours-in-a-d3-force-directed-layout-2/
+//     // we need to create an array of "connections" for quick lookup
+//
+//   });
 
-  $.when(forceData,buildNetwork(forceData.responseJSON));
+/* ------------ Network Generation Block ----------*/
+//set color scale
+var color = d3.scaleOrdinal()
+  .domain(allDomains)
+  .range(networkChart.selectedColorPalette);
 
+function updateColorPalette() {
   var color = d3.scaleOrdinal()
     .domain(allDomains)
-    .range(networkChart['selectedColorPalette']);
+    .range(networkChart.selectedColorPalette);
+}
 
-  var parentDiv = document.getElementById("viz-1");
-  var width = parentDiv.clientWidth;
-  var height = parentDiv.clientHeight;
+// define a "lookup" function to get connections
+function neighboring(a, b) {
+  return linkedByIndex[a.index + "," + b.index];
+};
+links = d3.selectAll('line');
 
+// set opacity based on connections
+function connectedNodes() {
+  if (toggle == 0) {
+    // select the nodes
+    d = d3.select(this).node().__data__;
+    // loop over each node and style
+    node.style("opacity", function(o) {
+      // ? is a conditional operator of the form condition ? value-if-true : value-if-false
+      return neighboring(d, o) | neighboring(o, d) ? 1 : 0.15;
+    });
+    toggle = 1;
+  } else {
+    node.style("opacity", 1);;
+    toggle = 0;
+  }
+};
 
-  networkChart['vis'] = d3.select("#viz-1")
-    .append("svg")
-    .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", "0 0 " + width + " " + height)
-    .call(d3.zoom().on("zoom", function() {
-      svg.attr("transform", d3.event.transform)
-    }))
-    .on("dblclick.zoom", null)
-    .append("g");
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+};
 
-  networkChart.force = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) {
-      return d.name
-    }).strength(1))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(w / 2, h / 2))
-    .force('collision', d3.forceCollide().radius(function(d) {
-      return (d.betweenesscentrality_1745 * 150 + d.degree_1745) / 2 + 15
-    }));
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+};
 
-  var link = networkChart['vis'].append("g")
-    .attr("class", "links")
-    .selectAll("line")
-    .data(networkChart.links)
-    .enter()
-    .append("line");
-  //.attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+};
 
-  var node = networkChart['vis'].selectAll("circle")
-    .data(networkChart.nodes)
-    .enter().append("circle")
+function refresh(nodes) {
+
+  // transition
+  var t = d3.transition()
+    .duration(750);
+
+  // JOIN new nodes with old nodes
+  node = node.data(nodes, function(d) {
+    return d.name;
+  });
+  node.exit()
+    .style("fill", "#b26745")
+    .transition(t)
+    .attr("r", 1e-6)
+    .remove();
+
+  node
+    .transition(t);
+
+  node.enter().append("circle")
     .attr("id", function(d) {
       return d.name + "-1"
     })
     .attr("r", function(d) {
-        return d.betweenesscentrality_1745 * 150 + 8;
+      return d.betweenesscentrality_1745 * 150 + 8;
     })
     .attr("fill", function(d) {
-      return color(d[networkChart['colorFillCategory']]);
+      return color(d[networkChart.colorFillCategory]);
     })
     .on("mouseover", function(d) {
-      div.html("<h6>" + d.name + "</h6><strong>Race:</strong> " + d.race + "</br><strong>Gender:</strong> " + d.gender + "</br><strong>Clan:</strong> " + d.clan + "</br><strong>First appeared as:</strong> " + d.role + "</br><strong>Subcommunity:</strong> " + d.modularity_class)
+      div.html("<h6>" + d.name + "</h6><strong>Race:</strong> " + d.race + "</br><strong>Gender:</strong> " + d.gender + "</br><strong>Clan:</strong> " + d.clan + "</br><strong>First appeared as:</strong> " + d.role +
+          "</br><strong>Subcommunity:</strong> " + d.modularity_class)
         .style("left", (d3.event.pageX) + "px")
         .style("top", (d3.event.pageY - 28) + "px")
         .style("opacity", 1);
@@ -153,12 +184,14 @@ function drawNetwork() {
       .on("end", dragended))
     .on('dblclick', connectedNodes);
 
-  var text = networkChart['vis'].selectAll("text")
+  console.log(node)
+
+  text = svg.selectAll("text")
     .data(node)
     .enter()
     .append("text");
 
-  var labels = text
+  labels = text
     .attr("x", function(d) {
       return d.cx
     })
@@ -169,42 +202,13 @@ function drawNetwork() {
       return "( " + d.cx + ", " + d.cy + " )"
     })
 
-  networkChart.force
+
+  simulation
     .nodes(networkChart.nodes)
     .on("tick", ticked);
-
-  networkChart.force
-    .force("link")
+  simulation.force("link")
     .links(networkChart.links);
 
-  function ticked() {
-    link
-      .attr("x1", function(d) {
-        return d.source.x;
-      })
-      .attr("y1", function(d) {
-        return d.source.y;
-      })
-      .attr("x2", function(d) {
-        return d.target.x;
-      })
-      .attr("y2", function(d) {
-        return d.target.y;
-      });
-    node
-      .attr("cx", function(d) {
-        return d.x;
-      })
-      .attr("cy", function(d) {
-        return d.y;
-      });
-
-
-
-    // mouse double click highlight is taken from this example:
-    // http://www.coppelia.io/2014/06/finding-neighbours-in-a-d3-force-directed-layout-2/
-    // we need to create an array of "connections" for quick lookup
-    var linkedByIndex = {};
     for (i = 0; i < networkChart.nodes.length; i++) {
       linkedByIndex[i + "," + i] = 1;
     };
@@ -213,45 +217,4 @@ function drawNetwork() {
     networkChart.links.forEach(function(d) {
       linkedByIndex[d.source.index + "," + d.target.index] = 1;
     });
-
-    // define a "lookup" function to get connections
-    function neighboring(a, b) {
-      return linkedByIndex[a.index + "," + b.index];
-    };
-    links = d3.selectAll('line');
-
-    // set opacity based on connections
-    function connectedNodes() {
-      if (toggle == 0) {
-        // select the nodes
-        d = d3.select(this).node().__data__;
-        // loop over each node and style
-        node.style("opacity", function(o) {
-          // ? is a conditional operator of the form condition ? value-if-true : value-if-false
-          return neighboring(d, o) | neighboring(o, d) ? 1 : 0.15;
-        });
-        toggle = 1;
-      } else {
-        node.style("opacity", 1);;
-        toggle = 0;
-      }
-    };
-  };
-};
-
-function dragstarted(d) {
-  if (!d3.event.active) networkChart.viz.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
-};
-
-function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
-};
-
-function dragended(d) {
-  if (!d3.event.active) networkChart.viz.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
-};
+}
